2729fe1366fa45792ffbed76b8b76f19728205b4
“vkay21” <vokerayee@gmail.com>
2023-03-18 11:03:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * this is a short program
+ * to write a short program
+ */
+
+Int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
