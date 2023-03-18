382c0c5bc7a0eaed785b4b0847f5b305d4efd607
“vkay21” <vokerayee@gmail.com>
2023-03-18 11:23:22 -0700

    betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * this is a short program
- * to write a short program
+ * main - print func replace with puts
+ * Return -Return: 0 if exited properly, non-zero otherwise always (0)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/*return always (0)*/
 	return (0);
 }
