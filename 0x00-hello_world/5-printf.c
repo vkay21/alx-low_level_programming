80514a2df820416a40aefa7166cfbe4d52c7ae08
“vkay21” <vokerayee@gmail.com>
2023-03-18 11:42:45 -0700

    betty errors

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - use printf
+ *
+ * Return: always 0 when condition is met
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retun(0);
+
+}
