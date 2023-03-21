5a447ee316d30b4219c227ba796f74107215e6f5
“vkay21” <vokerayee@gmail.com>
2023-03-21 09:30:13 -0700

    print alphabet 10times

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
 	char n; /*declare a variable (n) to store the alphabets*/
 
-	for (n = 'a'; ch <= 'z'; ++n)
+	for (n = 'a'; n <= 'z'; ++n) /*loops through the var */
 		_putchar(n);
 	_putchar('\n');
 }
