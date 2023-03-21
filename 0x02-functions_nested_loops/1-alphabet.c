979a4c93372c8bbf60a397ebc4907787ee9986f5
“vkay21” <vokerayee@gmail.com>
2023-03-21 09:16:10 -0700

    funnc prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - func prints alphabet in lower case
+ *
+ * Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char n; /*declare a variable (n) to store the alphabets*/
+
+	for (n = 'a'; ch <= 'z'; ++n)
+		_putchar(n);
+	_putchar('\n');
+}
