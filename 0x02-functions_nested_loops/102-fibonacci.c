5bb16eb13e5741cf2c48dc9b245952ab7ba5d4e8
Voke <vokerayee@gmail.com>
2023-03-22 10:32:06 -0700

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,8 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Description: print the first 100 fibonacci numbers
+ *
+ * Return: 0 (success)
  */
 int main(void)
+{
+	long prev, curr, temp;
+	int i;
+
+	i = 1;
+	prev = 1;
+	curr = 2;
+	printf("%ld, %ld, ", prev, curr);
+	while (i <= 48)
+	{
+		temp = curr;
+		curr += prev;
+		prev = temp;
+		printf("%ld", curr);
+		if (i != 48)
+			printf(", ");
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
