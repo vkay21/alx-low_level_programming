486728c7ff067508316ecd49b2f53c0709299ef5
Voke <vokerayee@gmail.com>
2023-03-22 10:29:49 -0700

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,33 @@
 #include "main.h"
 
 /**
- * main - check the code
+ *print_times_table - print a times table for a given number n
+ * @n: the maximum number
  *
- * Return: Always 0.
  */
-int main(void)
+void print_times_table(int n)
+{
+	int i, j, product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; ++i)
+		{
+			for (j = 0; j <= n; ++j)
+			{
+				product = i * j;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product < 100)
+						_putchar(' ');
+					if (product < 10)
+						_putchar(' ');
+				}
+				if (product >= 100)
+				{
+					_putchar('0' + product / 100);
+					_putchar('0' + (product % 100) / 10);
+					_putchar('0' + (product % 10));
+				}
