43982d0e96739c5aa012984591d7ed9a015e4550
Voke <vokerayee@gmail.com>
2023-03-22 14:41:50 -0700

    func to print natural nnumbers from 0-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * print_to_98 - print a sequence starting from a give number to 98
+ * @n: starting number
  *
- * Return: Always 0.
  */
-int main(void)
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d", n);
+		if (n > 98)
+			--n;
+		else if (n < 98)
+			++n;
+		printf(", ");
+	}
+	printf("%d", 98);
+	printf("\n");
+}
