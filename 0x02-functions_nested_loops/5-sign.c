e6123c034d3d4cc107776558270937c977925539
Voke <vokerayee@gmail.com>
2023-03-22 10:18:55 -0700

    5-sign.c checks if a number is neg or positive

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,26 @@
-#include "main.h"
+include "main.h"
 
 /**
- * main - check the code.
+ * print_sign - print the sign of a number
+ * @n: the number to be checked
  *
- * Return: Always 0.
+ * Return: 1 if number is positive, 0 if number is 0, otherwise -1
  */
-int main(void)
+int print_sign(int n)
 {
-    int r;
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
